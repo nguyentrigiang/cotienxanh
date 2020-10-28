f3a32fc9fa41a81f9d5ad26ed17bc83970a2901c
Nguyen Tri Giang <nguyentrigiang1991@gmail.com>
2020-10-28 23:19:06 +0700

    First commit

---
+++ src/components/DiamondBadge/DiamondText.js
@@ -0,0 +1,40 @@
+import Phaser from 'phaser'
+import User from '../User'
+import store from 'store'
+
+class DiamondText extends Phaser.GameObjects.Text {
+  constructor (scene, addToScene = true, config = {}) {
+    const x = 608
+    const y = 48
+    const { diamond } = User.currentUser()
+
+    super(scene, x, y, diamond)
+
+    Phaser.GameObjects.BuildGameObject(scene, this, { ...config, x, y })
+    this.setOrigin(0, 0.5)
+    this.setScrollFactor(0)
+    this.setAlign('left')
+    this.setFontFamily('Quicksand, monospace')
+    this.setFontStyle('bold')
+    this.setColor('#4FC3F7')
+    this.setShadow(1, 1, '#000000')
+    this.diamond = diamond
+
+    if (addToScene) this.addToScene(scene)
+  }
+
+  addToScene (scene) {
+    scene.add.existing(this)
+  }
+
+  updateDiamond () {
+    this.setText(this.diamond)
+  }
+
+  refreshDiamond () {
+    var diamond = store.get(diamond)
+    this.setText(diamond)
+  }
+}
+
+export default DiamondText
